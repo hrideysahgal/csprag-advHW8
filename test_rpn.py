fbf40b236fb1a7157ef74234e86f994ce1cdffa5
Hridey Sahgal <hridey@umich.edu>
2019-03-08 00:06:56 -0500

    Added tests for copy functionality, copy functionality tests failing

--- test_rpn.py
+++ test_rpn.py
@@ -17,3 +17,6 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate("2 4 ^")
         self.assertEqual(16, result)
+    def test_copy(self):
+        result = rpn.calculate("2 4 c +")
+        self.assertEqual(8, result)
